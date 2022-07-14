4456336551efc6a74b75505b4ea6ed55cc30c27f
yisha17 <it.yisak.wondim@gmail.com>
2022-07-14 18:52:07 +0300

    defining user model

---
+++ lib/models/user.dart
@@ -0,0 +1,52 @@
+class User {
+  final String? login;
+  final String? name;
+  final int? id;
+  final String? avatarUrl; //avatar_url
+  final int? publicRepos; // public_repos
+  final int? followers;
+  final int? following;
+  final String? twitterUsername; //twitter_username
+  final String? location;
+  final String? createdAt; //created_at
+  final String? bio;
+  final String? company;
+  final String? blog;
+
+  User(
+      {this.login,
+      this.name,
+      this.id,
+      this.avatarUrl,
+      this.publicRepos,
+      this.followers,
+      this.following,
+      this.twitterUsername,
+      this.location,
+      this.createdAt,
+      this.bio,
+      this.company,
+      this.blog});
+
+
+   factory User.fromJSON(Map<String,dynamic>jsonMap){
+    final result = User(
+      login: jsonMap['login'],
+      name: jsonMap['name'],
+      id: jsonMap['id'],
+      avatarUrl:jsonMap['avatar_url'],
+      publicRepos:jsonMap['public_repos'],
+      followers:jsonMap['followers'],
+      following:jsonMap['following'],
+      twitterUsername:jsonMap['twitter_username'],
+      location:jsonMap['location'],
+      createdAt:jsonMap['created_at'],
+      bio:jsonMap['bio'],
+      company:jsonMap['company'],
+      blog:jsonMap['blog']
+    );
+    return result;
+  }
+}
+
+
--- lib/repository/Search_repository.dart
+++ lib/repository/Search_repository.dart
@@ -3,6 +3,6 @@
 
 class SearchRepository{
 
-  Future<>
+
 
 }
